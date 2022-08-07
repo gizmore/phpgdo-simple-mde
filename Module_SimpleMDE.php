67f4de04ace2587b4ab1d8800258392f0b1eea64
gizmore <gizmore@wechall.net>
2022-08-07 06:28:18 +0200

    Init

---
+++ Module_SimpleMDE.php
@@ -0,0 +1,21 @@
+<?php
+namespace GDO\SimpleMDE;
+
+use GDO\Core\GDO_Module;
+
+/**
+ * SimpleMDE integration module.
+ * 
+ * @author gizmore
+ * @version 7.0.1
+ * @since 7.0.1
+ */
+final class Module_SimpleMDE extends GDO_Module
+{
+	public function onIncludeScripts() : void
+	{
+		
+	}
+	
+	
+}
